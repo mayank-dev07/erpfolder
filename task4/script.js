e494ea07e64515d55763c068573092e6ef76fdde
mayank_dev <123981067+mayank-dev07@users.noreply.github.com>
2023-07-09 23:14:58 +0530

    added

---
+++ task4/script.js
@@ -0,0 +1,40 @@
+let myApp=angular.module("myModule",[])
+
+myApp.controller("myController",function($scope){
+    $scope.List=JSON.parse(localStorage.getItem("data")) || [];
+    
+    $scope.addTodo = () => {
+        if($scope.newTodo){
+            let Todo = {
+                text: $scope.newTodo,
+                time: new Date(),
+                status: "added"
+            }
+
+            $scope.List.push(Todo);
+            $scope.newTodo='';
+            localStorage.setItem("data",JSON.stringify($scope.List));
+        }
+    }
+
+    $scope.remove = function(index){
+        $scope.List[index].status = "removed";
+        localStorage.setItem("data",JSON.stringify($scope.List));  
+    }
+    $scope.showRow = function (index) {
+        return $scope.List[index].status == "added";
+      };
+      
+    $scope.edit=function(index){
+        if($scope.newTodo){
+            $scope.List[index].text = $scope.newTodo;
+            $scope.List[index].time = new Date();
+            $scope.newTodo='';
+            localStorage.setItem("data",JSON.stringify($scope.List));
+        }
+        else{
+            alert("THERE IS NOTHING TO EDIT")
+        }
+    }
+});
+
